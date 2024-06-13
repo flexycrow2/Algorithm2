8b26ee2b2daad055124192544c546c319e4cd91d
flexycrow2 <163443735+flexycrow2@users.noreply.github.com>
2024-06-13 14:52:52 +0900

    [level 0] Title: 가위 바위 보, Time: 0.40 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(String rsp) {
         String[] arr = rsp.split("");
-        for(int i = 0; i < rsp.length(); i++){
+        for(int i = 0; i < arr.length; i++){
             if(arr[i].equals("2")) arr[i] = "0";
             else if(arr[i].equals("0")) arr[i] = "5";
             else if(arr[i].equals("5")) arr[i] = "2";
