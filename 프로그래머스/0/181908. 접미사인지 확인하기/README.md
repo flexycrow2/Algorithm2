ac4e29792c2b4041ffccd9c0d395c0a7d9435e98
flexycrow2 <163443735+flexycrow2@users.noreply.github.com>
2024-06-28 01:17:41 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.05 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/README.md
+++ 프로그래머스/0/181908. 접미사인지 확인하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 접미사인지 확인하기 - 181908 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181908) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181908#qna) 
 
 ### 성능 요약
 
-메모리: 73.2 MB, 시간: 0.04 ms
+메모리: 71.9 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 24일 21:30:15
+2024년 06월 28일 01:17:39
 
 ### 문제 설명
 
--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -1,22 +1,25 @@
 class Solution {
     public int solution(String my_string, String is_suffix) {
-        int answer = 0;
-        int a = my_string.length();
-        int b = is_suffix.length();
-        String answer1 = "";
-        if(a >= b){
-            answer1 = my_string.substring(a-b, a);
-        }else return answer;
-        if(answer1.equals(is_suffix)) answer = 1;
-        return answer;
         // int answer = 0;
-        // if(my_string.length() < is_suffix.length()) return answer;
+        // int a = my_string.length();
+        // int b = is_suffix.length();
+        // String answer1 = "";
+        // if(a >= b){
+        //     answer1 = my_string.substring(a-b, a);
+        // }else return answer;
+        // if(answer1.equals(is_suffix)) answer = 1;
+        // return answer;
+        int answer = 0;
+        if(my_string.length() < is_suffix.length()) return answer;
 
-//         StringBuilder sb = new StringBuilder(my_string);
-//         sb.reverse();
-//         String substring = sb.substring(0,is_suffix.length());
+        StringBuilder sb = new StringBuilder(my_string);
+        StringBuilder sb2 = new StringBuilder(is_suffix);
+        sb.reverse();
+        sb2.reverse();
+        String substring = sb.substring(0,is_suffix.length());
+        String substring2 = sb2.toString();
         
-//         if(substring.equals(is_suffix)) answer = 1;
-//         return answer;
+        if(substring.equals(substring2)) answer = 1;
+        return answer;
     }
 }
