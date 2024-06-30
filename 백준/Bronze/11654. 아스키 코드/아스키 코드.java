15c63b0f0d1f163fb75eff45980a77e0910efef5
flexycrow2 <163443735+flexycrow2@users.noreply.github.com>
2024-06-30 22:47:32 +0900

    [Bronze V] Title: 아스키 코드, Time: 172 ms, Memory: 17564 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char a = scanner.next().charAt(0);
+        int b = (int) a;
+        System.out.println(b);
+    }
+}
