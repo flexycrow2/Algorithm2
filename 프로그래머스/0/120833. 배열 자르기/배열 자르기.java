1fd3fef7190a5443ae30893e2c92562d44a2e5e6
flexycrow2 <163443735+flexycrow2@users.noreply.github.com>
2024-07-11 20:04:38 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 73.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120833. 배열 자르기/배열 자르기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int[] solution(int[] numbers, int num1, int num2) {
+        int[] answer = new int[num2 - num1 + 1];
+        putIn(answer, numbers, num1);
+        return answer;
+    }
+    
+    public void putIn(int[] answer, int[] numbers, int num1){
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = numbers[num1];
+            num1++;
+        }
+    }
+}
