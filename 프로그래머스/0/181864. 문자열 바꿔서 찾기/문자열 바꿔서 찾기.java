962c24a69b9d1e6e9a09280ba54d2d275d1d2b4f
flexycrow2 <163443735+flexycrow2@users.noreply.github.com>
2024-07-24 22:40:00 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.07 ms, Memory: 65.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < myString.length(); i++){
+            if(myString.charAt(i) == 'A') sb.append('B');
+            else sb.append('A');
+        }
+        String str = sb.toString();
+        if(str.contains(pat)) answer = 1;
+        return answer;
+    }
+}
