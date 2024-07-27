3b6d043e1707cd40d2f3ba8184313ea54c3727e2
flexycrow2 <163443735+flexycrow2@users.noreply.github.com>
2024-07-27 21:37:59 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.13 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
+++ 프로그래머스/0/120822. 문자열 뒤집기/문자열 뒤집기.java
@@ -1,9 +1,12 @@
 class Solution {
     public String solution(String my_string) {
-        String answer = "";
-        for(int i = my_string.length() - 1; i >= 0; i--){
-            answer += my_string.charAt(i);
-        }
-        return answer;
+        // String answer = "";
+        // for(int i = my_string.length() - 1; i >= 0; i--){
+        //     answer += my_string.charAt(i);
+        // }
+        // return answer;
+        StringBuilder sb = new StringBuilder(my_string);
+        sb.reverse();
+        return sb.toString();
     }
 }
