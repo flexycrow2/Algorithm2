e8bd1dba7ee2632101d6b48089c458ecea99eccb
flexycrow2 <163443735+flexycrow2@users.noreply.github.com>
2024-08-02 20:20:16 +0900

    [level 0] Title: 카운트 다운, Time: 0.03 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] solution(int s, int e) {
+        int[] answer = new int[s - e + 1];
+        for(int i = 0; i < answer.length; i++){
+            answer[i] = s--; //이 부분
+        }
+        return answer;
+    }
+}
